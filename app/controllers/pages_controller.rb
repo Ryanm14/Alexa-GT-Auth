41fb49480fd60c70166c823e2d72f2a44d9e4037
Ryan Miles <ryanm1114@gmail.com>
2017-10-14 04:03:39 +0000

    Fix state

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
   def home
+    $state = params[:state]
+    Rails.logger.debug("debug::" + $state)
   end
   
   def show
-    @state = Rack::Utils.parse_query URI(request.original_url).query[:state]
     @username = params[:username]
     @password = params[:password]
     redirect_to "https://layla.amazon.com/spa/skill/account-linking-status.html?vendorId=M3STMGS1241AEG#state=" + @state.to_s + "&access_token=" + @username.to_s + "," + @password.to_s + "&token_type=Bearer"
