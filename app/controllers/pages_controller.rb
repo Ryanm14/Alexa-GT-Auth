0c019de351547a0bb9247998504e040678100900
Ryan Miles <ryanm1114@gmail.com>
2017-10-14 03:33:06 +0000

    Change Token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   def show
     @username = params[:username]
     @password = params[:password]
-    redirect_to "https://layla.amazon.com/spa/skill/account-linking-status.html?vendorId=M3STMGS1241AEG#state=xyz&access_token=" + @username.to_s + "," + @password.to_s + "&token_type=Bearer"
+    redirect_to "https://layla.amazon.com/spa/skill/account-linking-status.html?vendorId=M3STMGS1241AEG#state=xyz&access_token=" + @username.to_s + "Q" + @password.to_s + "&token_type=Bearer"
   end
     
 end
