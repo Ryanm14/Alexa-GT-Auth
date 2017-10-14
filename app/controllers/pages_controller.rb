0c185f7f6848f441466fea5ba8e7e55b9522e83c
Ryan Miles <ryanm1114@gmail.com>
2017-10-14 03:44:49 +0000

    patch state

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @username = params[:username]
-    @username = params[:password]
   end
   
   def show
+    @state = Rack::Utils.parse_query URI(request.original_url).query[:state]
     @username = params[:username]
     @password = params[:password]
-    redirect_to "https://layla.amazon.com/spa/skill/account-linking-status.html?vendorId=M3STMGS1241AEG#state=xyz&access_token=" + @username.to_s + "Q" + @password.to_s + "&token_type=Bearer"
+    redirect_to "https://layla.amazon.com/spa/skill/account-linking-status.html?vendorId=M3STMGS1241AEG#state=" + @state.to_s + "&access_token=" + @username.to_s + "," + @password.to_s + "&token_type=Bearer"
   end
     
 end
